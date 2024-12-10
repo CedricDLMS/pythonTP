ff4bd24061dc5c958faf7fa6315f45dfc505f0e2
CedricDLMS <delmascede@gmail.com>
2024-12-10 11:40:40 +0100

    exo_3 fini

--- Exercice_3/main.py
+++ Exercice_3/main.py
@@ -9,7 +9,7 @@ total_classe_3 : int = 0
 survivant_classe_1 : int = 0
 survivant_classe_2 : int = 0
 survivant_classe_3 : int = 0
-bateau = dict()
+bateau = {}
 
 with open('./annexes/titanic_survival.csv', encoding='utf-8') as csvfiles:
     reader = csv.DictReader(csvfiles, delimiter=',')
@@ -27,6 +27,7 @@ with open('./annexes/titanic_survival.csv', encoding='utf-8') as csvfiles:
             survived = int(row['survived'])
             pclass = int(row['pclass'])
             
+            
             if pclass == 1:
                 total_classe_1 += 1
                 if survived == 1:
@@ -40,12 +41,51 @@ with open('./annexes/titanic_survival.csv', encoding='utf-8') as csvfiles:
                 if survived == 1:
                     survivant_classe_3 += 1
         except:
+            pass
+        
+        try :
+            bateau_row = int(row['boat'])
+            if bateau_row not in bateau:
+                bateau[bateau_row] = 1
+            else :
+                bateau[bateau_row] += 1
+        except:
+            pass
         
 
 poucentage_survivant_1 = (survivant_classe_1 / total_classe_1) * 100
 poucentage_survivant_2 = (survivant_classe_2 / total_classe_2) * 100
 poucentage_survivant_3 = (survivant_classe_3 / total_classe_3) * 100
 
-print(poucentage_survivant_1)
-print(poucentage_survivant_2)
-print(poucentage_survivant_3)
+bateau_max = max(bateau, key=bateau.get) #variables pour le bateau le plus performant 
+valeur_max = bateau[bateau_max]
+
+print("------------------------------------------------------------------------")
+print("---------------------------MOYENNE D'AGE--------------------------------")
+print("------------------------------------------------------------------------")
+
+moyenne_age = sum(ages) / len(ages)
+
+print(f"La moyenne d'age est : {moyenne_age:.2f}")
+
+print("------------------------------------------------------------------------")
+print("-----------------------POURCENTAGE DE SURVIE----------------------------")
+print("------------------------------------------------------------------------")
+
+print(f"Pourcentage de survivants en classe 1 : {poucentage_survivant_1:.2f}%")
+print(f"Pourcentage de survivants en classe 2 : {poucentage_survivant_2:.2f}%")
+print(f"Pourcentage de survivants en classe 3 : {poucentage_survivant_3:.2f}%")
+
+
+
+print("------------------------------------------------------------------------")
+print("-------------------------BATEAUX SAUVETEURS-----------------------------")
+print("------------------------------------------------------------------------")
+
+for cle,valeur in bateau.items():
+    print(f"dans le bateau : {cle} , nous avons {valeur} survivants")
+
+print("------------------------------------------------------------------------")
+print("-------------------------BATEAU LE PLUS GENIAL--------------------------")
+print("------------------------------------------------------------------------")
+print(f"Le bateau avec le plus de survivants est le bateau {bateau_max}, avec {valeur_max} survivants.")
